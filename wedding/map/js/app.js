c8d344f46ca39ab694528dd378c20a5431095e4c
Rainer Poisel <rainer.poisel@gmail.com>
2014-02-03 10:00:17 +0100

    major refactor

--- wedding/map/js/app.js
+++ wedding/map/js/app.js
@@ -18,119 +18,129 @@ var CustomIcon = L.Icon.extend(
     }
 });
 
-function instantiateMarkers(items, zoomLevel)
-{
-    var markerArray = new Array();
-    var newIcon = null;
-    $.each(items, function(index, element)
-    {
-        if (zoomLevel >= 17)
+(function (window, document, undefined) {
+    'use strict';
+    L.PartyMap = {};
+    L.PartyMap.version = '1.0';
+    L.PartyMap.Map = L.Map.extend({
+
+        addMarkers: function (items)
         {
-            newIcon = new CustomIcon(
+            var markerArray = new Array();
+            var newIcon = null;
+            var _theMap = this;
+            $.each(items, function(index, element)
             {
-                iconUrl: element.iconUrl
+                if (_theMap._zoom >= 17)
+                {
+                    newIcon = new CustomIcon(
+                    {
+                        iconUrl: element.iconUrl
+                    });
+                }
+                else
+                {
+                    newIcon = L.AwesomeMarkers.icon(
+                    {
+                        icon: element.iconType,
+                        markerColor: element.color,
+                        prefix: 'fa',
+                    });
+                }
+                var newMarker = L.marker([element.lat, element.lng],
+                {
+                    icon: newIcon
+                });
+                _theMap._bindPopup(
+                        newMarker, element.name, element.picture, element.description);
+                markerArray.push(newMarker);
             });
+            this._markers = markerArray;
+            this.addLayers(markerArray);
+        },
+        removeMarkers: function()
+        {
+            if (this._markers != null)
+            {
+                this.removeLayers(this._markers);
+            }
+            this._markers = null;
+        },
+        addPaths: function (paths)
+        {
+            var pathsArray = new Array();
+            var _theMap = this;
+
+            $.each(paths, function(index, element)
+            {
+                var pointList = new Array();
+                $.each(element.points, function(index, point)
+                {
+                    pointList.push(L.latLng(point.lat, point.lng));
+                });
+                var newPath = L.polyline(pointList,
+                    {
+                        color: element.color,
+                        weight: 6,
+                        oppacity: 0.5,
+                        smoothFactor: 1
+                    });
+                _theMap._bindPopup(
+                    newPath, element.name, element.picture, element.description);
+                pathsArray.push(newPath);
+            });
+            this._paths = pathsArray;
+            this.addLayers(pathsArray);
+        },
+        addLayers: function(items)
+        {
+            var _theMap = this;
+            $.each(items, function(index, item)
+            {
+                _theMap.addLayer(item);
+            });
+        },
+        removeLayers: function(items)
+        {
+            var _theMap = this;
+            $.each(items, function(index, item)
+            {
+                _theMap.removeLayer(item);
+            });
+        },
+        _bindPopup: function(element, name, picture, description)
+        {
+            element.bindPopup("<div class='infobox'>" +
+              "<h2>"+name+"</h2>" +
+              "<img src='" + picture +"' class='writerpic'/>"+
+              "<p class='writerdes'>"+description+"</p>" +
+              "<div style='clear:both;'></div>"+
+              "</div>");
         }
-        else
-        {
-            newIcon = L.AwesomeMarkers.icon(
-            {
-                icon: element.iconType,
-                markerColor: element.color,
-                prefix: 'fa',
-            });
-        }
-        var newMarker = L.marker([element.lat, element.lng],
-        {
-            icon: newIcon
-        });
-        newMarker.bindPopup("<div class='infobox'>" +
-                          "<h2>"+element.name+"</h2>" +
-                          "<img src='" + element.picture +"' class='writerpic'/>"+
-                          "<p class='writerdes'>"+element.description+"</p>" +
-                          "<div style='clear:both;'></div>"+
-                          "</div>");
-        markerArray.push(newMarker);
     });
-    return markerArray;
-}
 
-function instantiatePaths(paths)
-{
-    var pathsArray = new Array();
-
-    $.each(paths, function(index, element)
+    L.PartyMap.map = function(options)
     {
-        var pointList = new Array();
-        $.each(element.points, function(index, point)
-        {
-            pointList.push(L.latLng(point.lat, point.lng));
-        });
-        var newPath = L.polyline(pointList,
-            {
-                color: element.color,
-                weight: 6,
-                oppacity: 0.5,
-                smoothFactor: 1
-            });
-        newPath.bindPopup("<div class='infobox'>" +
-                          "<h2>"+element.name+"</h2>" +
-                          "<img src='" + element.picture +"' class='writerpic'/>"+
-                          "<p class='writerdes'>"+element.description+"</p>" +
-                          "<div style='clear:both;'></div>"+
-                          "</div>");
-        pathsArray.push(newPath);
-    });
-    return pathsArray;
-}
-
-function addMarkerLayers(markers, map)
-{
-    $.each(markers, function(index, marker)
-    {
-        map.addLayer(marker);
-    });
-}
-
-function removeMarkerLayers(markers, map)
-{
-    $.each(markers, function(index, marker)
-    {
-        map.removeLayer(marker);
-    });
-}
-
-function addPathLayers(p, map)
-{
-    $.each(p, function(index, path)
-    {
-        map.addLayer(path);
-    });
-}
+        return new L.PartyMap.Map(options);
+    };
+}(this, document));
 
 function main(data)
 {
-    var map = L.map('map');
+    var map = L.PartyMap.map('map');
     var added = true;
     var markers = null;
     var paths = null;
 
     map.on("load", function (loadArg)
     {
-        // add items to be shown to map
-        markers = instantiateMarkers(data.items, loadArg.target._zoom);
-        addMarkerLayers(markers, map);
-
-        // add paths
-        paths = instantiatePaths(data.paths);
-        addPathLayers(paths, map);
+        map.addPaths(data.paths);
     });
 
     map.on("zoomend", function (zoomArg)
     {
-        removeMarkerLayers(markers, map);
-        markers = instantiateMarkers(data.items, zoomArg.target._zoom);
-        addMarkerLayers(markers, map);
+        map.removeMarkers();
+        map.addMarkers(data.items);
     });
 
     map.setView(data.config.center, data.config.zoomLevel);
